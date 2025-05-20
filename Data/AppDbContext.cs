50689372cfc81f7b70251ea8413d88ea3428f21b
Edgar <edgar.muoni@gmail.com>
2025-05-20 22:15:28 +0300

    Update

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -10,6 +10,9 @@
         {
         }
         public virtual DbSet<City> Cities { get; set; }
+
         public virtual DbSet<Country> Countries { get; set; }
+
+        public virtual DbSet<Customer> Customers { get; set; }
     }
 }
--- obj/Debug/net8.0/AdvancedAjax.AssemblyInfo.cs
+++ obj/Debug/net8.0/AdvancedAjax.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdvancedAjax")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ae3cfe8d7c2ad5191795f596f3c36240d486e4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+215341a152b2947a63dfc24c5e219e7c0c9d1b5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdvancedAjax")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdvancedAjax")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
